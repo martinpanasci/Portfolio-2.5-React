8db77a41ed72380cbaf9fb3f245d90fe2bfe76f8
Martin Panasci <mpanasci95@gmail.com>
2025-04-14 21:29:14 -0300

    Error de prueba con source map activo

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
@@ -5,9 +6,12 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    svgr(),
-  ],
+  plugins: [react(), tailwindcss(), svgr(), sentryVitePlugin({
+    org: "yo-wq",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
