bb9081b3faee43ab2f0dc61ab2b9db2b1ac56aac
Martin Panasci <mpanasci95@gmail.com>
2025-04-14 22:03:28 -0300

    Configurar tracePropagationTargets en Sentry solo para dominio de producción

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ Sentry.init({
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/]
+  tracePropagationTargets: [/^https:\/\/portfolio-2-5-react\.vercel\.app/]
 });
 
 
