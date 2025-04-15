864c61ce8e6b4812b5cd613dd40fb7971e7975a9
Martin Panasci <mpanasci95@gmail.com>
2025-04-14 22:00:24 -0300

    agregue tracking de sentry al main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,14 @@ import { LanguageProvider } from './context/LanguageContext'
 
 // Inicializás Sentry con el DSN que te dieron
 Sentry.init({
-  dsn: "https://07e8f76abb82c0f910c4e03f35f17357@o4509154301313024.ingest.us.sentry.io/4509154528133120"
+  dsn: "https://07e8f76abb82c0f910c4e03f35f17357@o4509154301313024.ingest.us.sentry.io/4509154528133120",
+  integrations: [
+    Sentry.browserTracingIntegration()
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/]
 });
 
 
